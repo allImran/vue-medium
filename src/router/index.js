98dc4abeef4bbafd61fadd6b82168d12836c3179
allImran <alemrandev@gmail.com>
2023-12-04 11:34:44 +0600

    test

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/reactivity',
+      name: 'reactivity',
+      component: () => import('../views/ReactivityView.vue')
     }
   ]
 })
---
+++ src/views/ReactivityView.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref, computed } from 'vue'
+import { useDebouncedRef } from '../composable/useDebouncedRef'
+
+const counter = ref(0)
+
+// const config = useDebouncedRef({
+//     title: 'Color',
+//     bgColor: 'green'
+// })
+
+const text = useDebouncedRef({title: 'hello'})
+
+// const bgColor = computed(() => {
+//     counter.value ++
+//     return config.value.bgColor
+// })
+
+</script>
+
+<template>
+    <!-- <h1>Reactivity Advanced</h1>
+    <h2>Count: {{ counter }}</h2>
+    <input type="color" v-model="config.bgColor">
+    <div :style="{
+        backgroundColor: bgColor
+    }">
+        {{ config.title }}
+    </div> -->
+    {{  text }}
+
+    <input v-model="text.title" />
+</template>
+
+
+<style scoped>
+div{
+    height: 100px;
+    width: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+
